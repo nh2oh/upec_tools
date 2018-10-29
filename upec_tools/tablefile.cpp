db9cd0e32b1440dca8f90642c5cd53c5414e4b06
nh2oh <ben@ionophore.com>
2018-10-29 14:39:54 -0500

    init

---
+++ upec_tools/tablefile.cpp
@@ -0,0 +1,131 @@
+#include "tablefile.h"
+#include <string>
+//#include <sstream>
+#include <filesystem>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+tablefile read_tablefile(const std::filesystem::path& fp) {
+	std::fstream fs {fp,std::ios::in};
+	if (!fs.is_open() || fs.fail()) {
+		std::cout << "Error opening file:  " << std::filesystem::absolute(fp) << std::endl;
+		std::abort();
+	}
+
+	
+	std::string delim {'\t'};
+	std::string cl {};
+	tablefile res {};  res.file = fp;
+	
+	std::getline(fs, cl);  // Skip the first line, assumed to be col headers
+	while (!fs.eof()) {
+		std::getline(fs,cl);
+		if (cl.size() == 0) {
+			continue;
+		};
+
+		tablefile_entry curr_tfe {};
+
+		size_t spos{0}; size_t epos{0};
+		epos = cl.find(delim,spos);
+		curr_tfe.contig_id = cl.substr(spos,epos);
+		cl.erase(0, epos+delim.size());
+		
+		epos = cl.find(delim,spos);
+		curr_tfe.feature_id = cl.substr(spos,epos);
+		cl.erase(0, epos+delim.size());
+
+		epos = cl.find(delim, spos);
+		curr_tfe.type = cl.substr(spos,epos);
+		cl.erase(0, epos+delim.size());
+
+		epos = cl.find(delim, spos);
+		curr_tfe.location = cl.substr(spos, epos);
+		cl.erase(0, epos+delim.size());
+
+		epos = cl.find(delim, spos);
+		if (epos == std::string::npos) {
+			break;
+		}
+		curr_tfe.start = std::stoi(cl.substr(spos, epos));
+		cl.erase(0, epos+delim.size());
+
+		epos = cl.find(delim, spos);
+		curr_tfe.stop = std::stoi(cl.substr(spos, epos));
+		cl.erase(0, epos+delim.size());
+
+		epos = cl.find(delim, spos);
+		curr_tfe.strand = cl.substr(spos,epos)=="+" ? true : false;
+		cl.erase(0, epos+delim.size());
+
+		epos = cl.find(delim, spos);
+		curr_tfe.function = cl.substr(spos, epos);
+		cl.erase(0, epos+delim.size());
+
+		epos = cl.find(delim, spos);
+		curr_tfe.aliases = cl.substr(spos, epos);
+		cl.erase(0, epos+delim.size());
+
+		epos = cl.find(delim, spos);
+		curr_tfe.figfam = cl.substr(spos, epos);
+		cl.erase(0, epos+delim.size());
+
+		epos = cl.find(delim, spos);
+		curr_tfe.evidence_codes = cl.substr(spos, epos);
+		cl.erase(0, epos+delim.size());
+
+		epos = cl.find(delim, spos);
+		curr_tfe.nucleotide_sequence = cl.substr(spos, epos);
+		cl.erase(0, epos+delim.size());
+
+		epos = cl.find(delim, spos);
+		curr_tfe.aa_sequence = cl.substr(spos, epos);
+		cl.erase(0, epos+delim.size());
+
+		res.data.push_back(curr_tfe);
+	}
+
+	return res; // {false,"true"};
+}
+
+
+bool check_tablefile(const tablefile& tf) {
+	
+	for (const auto& e : tf.data) {
+		if (std::abs(e.stop-e.start)+1 != e.nucleotide_sequence.size()) {
+			// [e.start,e.stop], not [e.start,e.stop)
+			return false;
+		}
+
+		if ((e.stop<e.start && e.strand) || (e.stop>e.start && !e.strand)) {
+			// Though in theory an element could span the nt defined as 0 and still be
+			// on the + strand...
+			return false;
+		}
+
+		bool is_protein {e.type.find("peg")!=std::string::npos};
+		bool is_rna {e.type.find("rna")!=std::string::npos};
+		if ((is_protein && is_rna) || (!is_protein && !is_rna)) {
+			return false;
+		}
+		if (is_rna) {
+			if (e.aa_sequence.size() != 0) {
+				return false;
+			}
+		}
+
+		if (is_protein) {
+			if (e.nucleotide_sequence.size()%3 != 0) {
+				return false;
+			}
+
+			if (e.aa_sequence.size()*3+3 != e.nucleotide_sequence.size()) {
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
---
+++ upec_tools/tablefile.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <string>
+#include <filesystem>
+#include <vector>
+
+struct tablefile_entry {
+	std::string contig_id {};
+	std::string feature_id {};
+	std::string type {};
+	std::string location {};
+	int64_t start {};
+	int64_t stop {};
+	bool strand {};
+	std::string function {};
+	std::string aliases {};
+	std::string figfam {};
+	std::string evidence_codes {};
+	std::string nucleotide_sequence {};
+	std::string aa_sequence {};
+};
+
+struct whatever {
+	bool yay {};
+	std::string what {};
+};
+
+struct tablefile {
+	std::filesystem::path file {};
+	std::vector<tablefile_entry> data {};
+};
+
+tablefile read_tablefile(const std::filesystem::path&);
+
+bool check_tablefile(const tablefile&);
+
+
+
+
